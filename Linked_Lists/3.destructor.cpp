b7beddb0ae420f217266e112a224547f69d7d0f3
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-09-28 13:54:28 +0530

    Create 3.destructor.cpp

---
+++ Linked_Lists/3.destructor.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// NODE CLASS
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        next = nullptr;
+    }
+};
+
+// LINKEDLIST CLASS (CLASSES ARE BY DEFAULT PRIVATE)
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    // CONSTRUCTOR
+    LinkedList(int value)
+    {
+        Node *newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    // PRINTLIST FUNCTION
+    void printList()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+            // move to the next node
+        }
+    }
+    
+    // DESTRUCTOR
+    ~LinkedList(){
+        Node* temp = head;
+        while(head){
+            head=head->next;
+            delete temp;
+            temp=head;
+        }
+    }
+};
+
